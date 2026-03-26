b68a42b257a98810133b1402f56c8b4d730d529c
HX <hee@HXdeMac-mini.local>
2026-03-26 09:42:33 +0800

    feat: expose getConfig/setConfig in preload

--- preload.js
+++ preload.js
@@ -1,5 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  openFile: () => ipcRenderer.invoke('open-file'),
+  openFile:  () => ipcRenderer.invoke('open-file'),
+  getConfig: () => ipcRenderer.invoke('get-config'),
+  setConfig: (patch) => ipcRenderer.invoke('set-config', patch),
 })
